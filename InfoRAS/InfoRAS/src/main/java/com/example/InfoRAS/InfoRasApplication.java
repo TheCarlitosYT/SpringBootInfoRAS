50254aac0487a878322cd825d246f221892b5540
Carlos Marín <cmarinbotella@gmail.com>
2024-02-28 11:15:20 +0100

    Test

---
+++ InfoRAS/InfoRAS/src/main/java/com/example/InfoRAS/InfoRasApplication.java
@@ -0,0 +1,13 @@
+package com.example.InfoRAS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoRasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoRasApplication.class, args);
+	}
+
+}
---
+++ InfoRAS/InfoRAS/src/test/java/com/example/InfoRAS/InfoRasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InfoRAS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoRasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
